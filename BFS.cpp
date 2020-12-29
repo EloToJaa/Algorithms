9451da3ac45fe7a180d62d0208e0eee0e03e9681
EloToJa <lukaszbudziak122@gmail.com>
2020-12-29 14:38:46 +0100

    algorytmy

--- BFS.cpp
+++ BFS.cpp
@@ -1,29 +1,39 @@
 #include<bits/stdc++.h>
-#define N 100002
 using namespace std;
-vector<int> v[N];
-bitset<N> o;
-queue<int> q;
-void bfs(int p) {
-    q.push(p);
-    while(!q.empty()) {
-        p = q.front();
-        q.pop();
-        for(int i = 0; i < v[p].size(); ++i) {
-            int u = v[p][i];
-            if(!o[u]) {
-                q.push(u);
-                o[u] = 1;
+#define FOR(i,a,b) for(int i = (a); i <= (b); ++i)
+#define FORD(i,a,b) for(int i = (b); i >= (a); --i)
+#define TRAV(x,T) for(auto& x: (T))
+#define ALL(T) T.begin(), T.end()
+#define TAB(T,a,b) (T)+a, (T)+((b)+1)
+#define VAR(x) #x<<" = "<<x<<" " 
+#define sz(x) (int)(x).size()
+#define nwd __gcd
+#define pb push_back
+#define st first
+#define nd second
+#define lc (v<<1)
+#define rc (v<<1|1)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+#define deb if(0)
+const int N = 1e6, NT = N + 2;
+
+vi V[NT];
+bitset<NT> vis;
+queue<int> Q;
+void bfs(int v) {
+    Q.push(v);
+    while(!Q.empty()) {
+        v = Q.front();
+        Q.pop();
+        TRAV(u, V[v]) {
+            if(!vis[u]) {
+                Q.push(u);
+                vis[u] = true;
             }
         } 
     }
-}
-signed main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    bfs(1);
-    for() {
-        
-    }
-    return 0;
 }
--- DFS.cpp
+++ DFS.cpp
@@ -1,20 +1,31 @@
 #include<bits/stdc++.h>
-#define N 100002
 using namespace std;
-vector<int> v[N];
-bitset<N> o;
-void dfs(int p) {
-    o[p] = 1;
-    for(int i = 0; i < v[p].size(); ++i) {
-        int u = v[p][i];
-        if(!o[u]) {
-            dfs(u);
-        }
-    } 
-}
-signed main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    dfs(1);
-    return 0;
+#define FOR(i,a,b) for(int i = (a); i <= (b); ++i)
+#define FORD(i,a,b) for(int i = (b); i >= (a); --i)
+#define TRAV(x,T) for(auto& x: (T))
+#define ALL(T) T.begin(), T.end()
+#define TAB(T,a,b) (T)+a, (T)+((b)+1)
+#define VAR(x) #x<<" = "<<x<<" " 
+#define sz(x) (int)(x).size()
+#define nwd __gcd
+#define pb push_back
+#define st first
+#define nd second
+#define lc (v<<1)
+#define rc (v<<1|1)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+#define deb if(0)
+const int N = 1e6, NT = N + 2;
+
+vi V[NT];
+bitset<NT> vis;
+void dfs(int v) {
+    vis[v] = true;
+    TRAV(u, V[v]) {
+        if(!vis[u]) dfs(u);
+    }
 }
