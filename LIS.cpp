b8e6a182cd63a23d1a255530e5d0b630dd27beaa
EloToJa <lukaszbudziak122@gmail.com>
2021-01-06 21:14:45 +0100

    algorytmy

--- LIS.cpp
+++ LIS.cpp
@@ -1,22 +1,26 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
-#define FORD(i, a, b) for (int i = (b); i >= (a); --i)
-#define TRAV(x, T) for (auto &(x) : (T))
+#define FOR(i,a,b) for(int i = (a); i <= (b); ++i)
+#define FORD(i,a,b) for(int i = (b); i >= (a); --i)
+#define TRAV(x,T) for(auto& x: (T))
 #define ALL(T) T.begin(), T.end()
-#define TAB(T, a, b) (T) + a, (T) + ((b) + 1)
-#define VAR(x) #x << " = " << x << " "
-#define sz(x) (int)(x).size()
+#define TAB(T,a,b) (T)+a, (T)+((b)+1)
+#define VAR(x) #x<<" = "<<x<<" " 
+#define SZ(x) (int)(x).size()
 #define nwd __gcd
 #define pb push_back
 #define st first
 #define nd second
+#define lc (v<<1)
+#define rc (v<<1|1)
 typedef long long ll;
 typedef long double ld;
 typedef pair<int, int> pii;
+typedef pair<int, ll> pil;
+typedef pair<ll, int> pli;
 typedef pair<ll, ll> pll;
 typedef vector<int> vi;
-#define deb if (0)
+#define deb if(0)
 const int N = 1e6, NT = N + 2;
 const int INF = 1e9 + 1;
 
