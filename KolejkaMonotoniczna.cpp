f983efa449faebc501f39e2df055ede117e4fb4e
EloToJa <lukaszbudziak122@gmail.com>
2021-01-06 13:26:16 +0100

    algorytmy

--- KolejkaMonotoniczna.cpp
+++ KolejkaMonotoniczna.cpp
@@ -1,16 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define FOR(i,a,b) for(int i = (a); i <= (b); ++i)
+#define FORD(i,a,b) for(int i = (b); i >= (a); --i)
+#define TRAV(x,T) for(auto& x: (T))
+#define ALL(T) T.begin(), T.end()
+#define TAB(T,a,b) (T)+a, (T)+((b)+1)
+#define VAR(x) #x<<" = "<<x<<" " 
+#define sz(x) (int)(x).size()
+#define nwd __gcd
+#define pb push_back
 #define st first
 #define nd second
-typedef pair<int, int> pii; 
-const int INF = 1e9 + 2;
+#define lc (v<<1)
+#define rc (v<<1|1)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, ll> pil;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+#define deb if(0)
+const ll INF = (ll)1e18 + 2;
 
 class monqueue {
 private:
     int pops, pushes;
 public:
-    deque<pii> Q;
-    void push(int a) {
+    deque<pli> Q;
+    void push(ll a) {
         while(!Q.empty() && Q.back().st <= a) Q.pop_back();
         pushes++;
         Q.push_back({a, pushes});
@@ -19,7 +37,7 @@ public:
         pops++;
         if(pops == Q.front().nd) Q.pop_front();
     }
-    int max() {
+    ll max() {
         if(Q.empty()) return -INF;
         return Q.front().st;
     }
@@ -30,8 +48,6 @@ monqueue Q;
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int n;
-    cin>>n;
-    Q.push(n);
+    
     return 0;
 }
