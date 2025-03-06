ee5e9f4a6b2218762046e82f38100252f4179750
EloToJaa <elotoja@protonmail.com>
2025-03-06 19:36:01 +0100

    Solve Mo

--- Mo.cpp
+++ Mo.cpp
@@ -1,84 +1,78 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-#define FOR(i,a,b) for(int i=(a); i<=(b); ++i)
-#define FORD(i,a,b) for(int i=(a); i>=(b); --i)
-#define TRAV(x,T) for(auto& (x): (T))
-#define ALL(x) x.begin(), x.end()
-#define TAB(x,n) (x)+1, (x)+((n)+1) 
-#define sz(x) (int)(x).size()
-#define pb push_back
-#define pf push_front
-#define st first
-#define nd second
-typedef long long ll;
-typedef long double ld;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
+
 const int N = 1e6;
 
 int A[N + 2], ILE[N + 2], ANS[N + 2];
-struct zap { int x, y, id; } Q[N + 2];
+struct zap {
+  int x, y, id;
+} Q[N + 2];
 
 int BL, ans = 0;
 int blok(int x) { return x / BL; }
 
 void add(int x) {
-    if(ILE[x] == 0) ++ans;
-    ILE[x]++;
+  if (ILE[x] == 0)
+    ++ans;
+  ILE[x]++;
 }
 
 void remove(int x) {
-    ILE[x]--;
-    if(ILE[x] == 0) --ans;
+  ILE[x]--;
+  if (ILE[x] == 0)
+    --ans;
 }
 
 int L = 0, R = 0;
 int query(int l, int r) {
-    while(R < r) {
-        ++R;
-        add(A[R]);
-    }
-    while(R > r) {
-        remove(A[R]);
-        --R;
-    }
-    while(L < l - 1) {
-        ++L;
-        remove(A[L]);
-    }
-    while(L >= l) {
-        add(A[L]);
-        --L;
-    }
-    return ans;
-}
-
-bool cmp(zap a, zap b) {
-    if(blok(a.x) != blok(b.x)) return a.x < b.x;
-    return a.y < b.y;
+  while (R < r) {
+    ++R;
+    add(A[R]);
+  }
+  while (R > r) {
+    remove(A[R]);
+    --R;
+  }
+  while (L < l - 1) {
+    ++L;
+    remove(A[L]);
+  }
+  while (L >= l) {
+    add(A[L]);
+    --L;
+  }
+  return ans;
 }
 
 signed main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
 
-    // wprowadzanie danych
-    int n, m;
-    cin>>n>>m;
-    FOR(i, 1, n) cin>>A[i];
-    FOR(i, 1, m) {
-        cin>>Q[i].x>>Q[i].y;
-        Q[i].id = i;
-    }
-    
-    // wyliczenie pierwiastka
-    BL = sqrt(n);
+  // wprowadzanie danych
+  int n, m;
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++)
+    cin >> A[i];
+  for (int i = 1; i <= n; i++) {
+    cin >> Q[i].x >> Q[i].y;
+    Q[i].id = i;
+  }
 
-    // sortowanie zapytan po blokach
-    sort(TAB(Q, m), cmp);
+  // wyliczenie pierwiastka
+  BL = sqrt(n);
 
-    FOR(i, 1, m) ANS[Q[i].id] = query(Q[i].x, Q[i].y);
-    FOR(i, 1, m) cout<<ANS[i]<<" "; cout<<"\n";
-    return 0;
+  // sortowanie zapytan po blokach
+  sort(Q + 1, Q + (m + 1), [](zap a, zap b) {
+    if (blok(a.x) != blok(b.x))
+      return a.x < b.x;
+    return a.y < b.y;
+  });
+
+  for (int i = 1; i <= m; i++)
+    ANS[Q[i].id] = query(Q[i].x, Q[i].y);
+  for (int i = 1; i <= m; i++)
+    cout << ANS[i] << " ";
+  cout << "\n";
+  return 0;
 }
