4f8270cda69b7749a45d55504b30bd93a291a798
EloToJaa <elotoja@protonmail.com>
2025-03-06 19:22:24 +0100

    Solve MonoQueue & Kruskal

--- KolejkaMonotoniczna.cpp
+++ KolejkaMonotoniczna.cpp
@@ -1,42 +1,24 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define FOR(i,a,b) for(int i = (a); i <= (b); ++i)
-#define FORD(i,a,b) for(int i = (b); i >= (a); --i)
-#define TRAV(x,T) for(auto& x: (T))
-#define ALL(T) T.begin(), T.end()
-#define TAB(T,a,b) (T)+a, (T)+((b)+1)
-#define VAR(x) #x<<" = "<<x<<" " 
-#define sz(x) (int)(x).size()
-#define nwd __gcd
-#define pb push_back
-#define st first
-#define nd second
-#define lc (v<<1)
-#define rc (v<<1|1)
-typedef long long ll;
-typedef long double ld;
-typedef pair<int, int> pii;
-typedef pair<int, ll> pil;
-typedef pair<ll, int> pli;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-#define deb if(0)
-const ll INF = (ll)1e18 + 2;
+#include <bits/stdc++.h>
 
-struct Monqueue {
-    int pops = 0, pushes = 0;
-    deque<pli> Q;
-    void push(const ll &val) {
-        while(!Q.empty() and Q.back().st <= val) Q.pop_back();
-        pushes++;
-        Q.push_back({val, pushes});
-    }
-    void pop() {
-        pops++;
-        if(!Q.empty() and pops == Q.front().nd) Q.pop_front();
-    }
-    ll max() {
-        if(Q.empty()) return -INF;
-        return Q.front().st;
-    }
+using namespace std;
+
+struct MonoQueue {
+  int pops = 0, pushes = 0;
+  deque<pair<long long, int>> Q;
+  void push(const long long &val) {
+    while (!Q.empty() and Q.back().first <= val)
+      Q.pop_back();
+    pushes++;
+    Q.push_back({val, pushes});
+  }
+  void pop() {
+    pops++;
+    if (!Q.empty() and pops == Q.front().second)
+      Q.pop_front();
+  }
+  long long max() {
+    if (Q.empty())
+      return -LONG_LONG_MAX;
+    return Q.front().first;
+  }
 };
--- Kruskal.cpp
+++ Kruskal.cpp
@@ -1,61 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-#define FOR(i,a,b) for(int i = (a); i <= (b); ++i)
-#define FORD(i,a,b) for(int i = (b); i >= (a); --i)
-#define TRAV(x,T) for(auto& x: (T))
-#define ALL(T) T.begin(), T.end()
-#define TAB(T,a,b) (T)+a, (T)+((b)+1)
-#define VAR(x) #x<<" = "<<x<<" " 
-#define sz(x) (int)(x).size()
-#define nwd __gcd
-#define pb push_back
-#define st first
-#define nd second
-#define lc (v<<1)
-#define rc (v<<1|1)
-typedef long long ll;
-typedef long double ld;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-#define deb if(0)
+
 const int N = 1e6, NT = N + 2;
 
 int REP[NT], ILE[NT];
 
 void Init(int n) {
-    FOR(i, 1, n) {
-        REP[i] = i;
-        ILE[i] = 1;
-    }
+  for (int i = 1; i <= n; i++) {
+    REP[i] = i;
+    ILE[i] = 1;
+  }
 }
 
 void Union(int a, int b) { // O(1)
-    if(ILE[a] > ILE[b]) swap(a, b);
-    REP[a] = b;
-    ILE[b] += ILE[a];
+  if (ILE[a] > ILE[b])
+    swap(a, b);
+  REP[a] = b;
+  ILE[b] += ILE[a];
 }
 
 int Find(int a) { // O(stala ackermana)
-    if(REP[a] != a) REP[a] = Find(REP[a]);
-    return REP[a];
+  if (REP[a] != a)
+    REP[a] = Find(REP[a]);
+  return REP[a];
 }
 
 struct Edge {
-    int a, b, w;
+  int a, b, w;
 } K[NT];
 
 void Kruskal(int n, int m) {
-    Init(n);
-    sort(TAB(K, 1, m), [](const Edge &a, const Edge &b) {
-        return a.w < b.w;
-    });
-    int a, b;
-    FOR(i, 1, m) {
-        a = Find(K[i].a);
-        b = Find(K[i].b);
-        if(a != b) {
-            Union(a, b);
-        }
+  Init(n);
+  sort(K + 1, K + (m + 1),
+       [](const Edge &a, const Edge &b) { return a.w < b.w; });
+  int a, b;
+  for (int i = 1; i <= m; i++) {
+    a = Find(K[i].a);
+    b = Find(K[i].b);
+    if (a != b) {
+      Union(a, b);
     }
+  }
 }
