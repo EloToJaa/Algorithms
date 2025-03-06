fa976c7030e01a638abf079e9a7660b0f39f3d66
EloToJaa <elotoja@protonmail.com>
2025-03-06 19:24:43 +0100

    Solve LCA

--- LCA.cpp
+++ LCA.cpp
@@ -1,53 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-#define FOR(i,a,b) for(int i = (a); i <= (b); ++i)
-#define FORD(i,a,b) for(int i = (b); i >= (a); --i)
-#define TRAV(x,T) for(auto& (x): (T))
-#define ALL(T) T.begin(), T.end()
-#define TAB(T,a,b) (T)+a, (T)+((b)+1)
-#define VAR(x) #x<<" = "<<x<<" " 
-#define sz(x) (int)(x).size()
-#define nwd __gcd
-#define pb push_back
-#define st first
-#define nd second
-#define lc (v<<1)
-#define rc (v<<1|1)
-typedef long long ll;
-typedef long double ld;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-#define deb if(0)
+
 const int N = 1e6, NT = N + 2;
 const int LOG = 20; // log n
 
-vi V[NT];
+vector<int> V[NT];
 int anc[NT][LOG + 1], pre[NT], post[NT], idx;
 
 void ancestors(int v, int p) {
-    anc[v][0] = p;
-    FOR(k, 1, LOG) {
-        anc[v][k] = anc[ anc[v][k - 1] ][k - 1];
-    }
-    idx++;
-    pre[v] = idx;
-    TRAV(u, V[v]) {
-        if(u != v) ancestors(u, v);
-    }
-    post[v] = idx;
+  anc[v][0] = p;
+  for (int k = 1; k <= LOG; k++) {
+    anc[v][k] = anc[anc[v][k - 1]][k - 1];
+  }
+  idx++;
+  pre[v] = idx;
+  for (const int &u : V[v]) {
+    if (u != v)
+      ancestors(u, v);
+  }
+  post[v] = idx;
 }
 
-bool przodek(int a, int b) { // czy a jest przodkiem b
-    return pre[a] <= pre[b] and pre[b] <= post[a];
+bool parent(int a, int b) { // czy a jest przodkiem b
+  return pre[a] <= pre[b] and pre[b] <= post[a];
 }
 
 int LCA(int a, int b) {
-    if(przodek(a, b)) return a;
-    if(przodek(b, a)) return b;
-    FORD(k, 0, LOG) {
-        if(!przodek(anc[a][k], b))
-            a = anc[a][k];
-    }
-    return anc[a][0];
+  if (parent(a, b))
+    return a;
+  if (parent(b, a))
+    return b;
+  for (int k = LOG; k >= 0; k--) {
+    if (!parent(anc[a][k], b))
+      a = anc[a][k];
+  }
+  return anc[a][0];
 }
